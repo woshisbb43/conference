dab97f9f681ed3b32546e554efc68304d8032465
nan <znbeebee@hotmail.com>
2018-06-19 19:16:28 +0800

    change position of resource

--- src/main/java/service/ServiceConfig.java
+++ src/main/java/service/ServiceConfig.java
@@ -1,7 +1,7 @@
 package service;
 
 public class ServiceConfig {
-    public static final String FILE_NAME = "resources/input_big";
+    public static final String FILE_NAME = "src/main/resources/input_big";
     public static final Double MINUTE_FOR_ONE_TRACK = 420.0;
     public static final Integer MORNING_SESSION_LENGTH = 180;
     public static final Integer AFTERNOON_SESSION_LENGTH = 240;
